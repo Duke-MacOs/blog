05a853a77d4223cf63d2d56d64b72a30d9cf0022
chenzhe <chenzhewangq@gmail.com>
2023-03-23 18:33:36 +0800

    feat: docs

--- docs/面试编程算法题/_test.js
+++ docs/面试编程算法题/_test.js
@@ -1,15 +1,18 @@
-const parseURL = (url) => {
-    const o = new URL(url);
-    const search = {};
-    for(let [key, value] of o.searchParams.entries()) {
-        search[key] = value;
-    }
-    return {
-        domain: o.host,
-        pathname: o.pathname,
-        search
-    }
+function cal(m) {
+	const arr1 = [100000, 80000, 60000, 40000, 20000, 5000, 2000, 500, 0];
+	const arr2 = [0.45, 0.4, 0.35, 0.3, 0.25, 0.2, 0.15, 0.1, 0.05];
+	let result = 0;
+
+	for(let i = 0; i < arr1.length; i++) {
+		const val = arr1[i];
+
+		if(m > val) {
+			const need = m - val;
+			result += need * arr2[i];
+			m -= need;
+		}
+	}
+	return result;
 }
 
-const url = 'https://segmentfault.com/a/1190000012113011?utm_source=tag-newest&name=Duke#123';
-console.log(parseURL(url));
+console.log(cal(123456))
